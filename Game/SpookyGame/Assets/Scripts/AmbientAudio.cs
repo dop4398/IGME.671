ba90dfe481eae284a52fd482f3716debc1f03e12
dop4398 <dop4398@rit.edu>
2020-04-20 03:24:33 -0400

    everything broke

---
+++ Game/SpookyGame/Assets/Scripts/AmbientAudio.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using FMODUnity;
+using UnityEngine;
+
+public class AmbientAudio : MonoBehaviour
+{
+    private StudioEventEmitter emitter;
+    private GameObject UI;
+
+    private int value;
+
+    void Start()
+    {
+        emitter = GetComponent<FMODUnity.StudioEventEmitter>();
+        UI = GameObject.Find("UI_Manager");
+        value = 2;
+    }
+
+    void Update()
+    {
+        if (emitter.EventInstance.isValid())
+        {
+            if (!UI.GetComponent<GameManager>().GetPauseEnabled())
+            {
+                value = 2; // full volume
+            }
+            else
+            {
+                value = 1; // paused snapshot
+            }
+            // Set the Ambient parameter
+            emitter.SetParameter("GameState", value);
+        }
+    }
+}
---
+++ Game/SpookyGame/Assets/Scripts/SkeletonAudio.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using FMODUnity;
+using UnityEngine;
+
+public class SkeletonAudio : MonoBehaviour
+{
+    private StudioEventEmitter emitter;
+    private GameObject player;
+    private GameObject UI;
+    private float radius;
+
+    void Start()
+    {
+        emitter = GetComponent<FMODUnity.StudioEventEmitter>();
+        //player = GameObject.FindGameObjectWithTag("Player");
+        UI = GameObject.Find("UI_Manager");
+        radius = 30;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!UI.GetComponent<GameManager>().GetPauseEnabled() && emitter.EventInstance.isValid())
+        {
+            //UpdateRattle();
+            emitter.SetParameter("GameState", 2);
+        }
+        else
+        {
+            emitter.SetParameter("GameState", 1);
+        }
+    }
+
+    void UpdateRattle()
+    {
+        float temp = 0;
+        float distance = DistanceToObjectSquared(player);
+
+        if (distance < radius * radius)
+        {
+            temp = 1 - (radius - Mathf.Sqrt(distance)) / radius;
+            // Set the distance to the player
+            emitter.SetParameter("DistanceToPlayer", temp);
+            print("Distance to player: " + temp);
+        }
+    }
+
+    float DistanceToObjectSquared(GameObject obj)
+    {
+        return (obj.transform.position - transform.position).sqrMagnitude;
+    }
+}
