ea2d9d6fd2e82159b1450f8e24aeb1039716fc77
dop4398 <dop4398@rit.edu>
2020-05-04 12:46:37 -0400

    aaa

---
+++ Game/WotBB/Assets/Scripts/CollisionAudio.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using FMODUnity;
+using UnityEngine;
+
+public class CollisionAudio : MonoBehaviour
+{
+    [SerializeField]
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Obstacle"))
+        {
+            GameObject.Find("WoodCollision").GetComponent<FMODUnity.StudioEventEmitter>().Play();
+        }
+        else if(collision.gameObject.CompareTag("Stone"))
+        {
+            GameObject.Find("StoneCollision").GetComponent<FMODUnity.StudioEventEmitter>().Play();
+        }
+        else if (collision.gameObject.CompareTag("Metal"))
+        {
+            GameObject.Find("MetalCollision").GetComponent<FMODUnity.StudioEventEmitter>().Play();
+        }
+    }
+}
